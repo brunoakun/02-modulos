524a8d83f4eebca4626b9125a6fb70d2213069c1
Bruno Barange <bruno.akun@gmail.com>
2022-07-18 12:27:18 +0200

    78-diseño

---
+++ src/app/datos.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatosService {
+  valor1: string = "Val 01";
+  valor2: string = "Val 02";
+  constructor() { }
+}
